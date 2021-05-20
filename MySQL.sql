4c448df7358760985c2eb880318fa736797845e6
brunocallachi <brunocallachi@hotmail.com>
2021-05-20 15:41:33 -0300

    MySQL

---
+++ MySQL.sql
@@ -0,0 +1,13 @@
+select * from tb_funcionaries;
+
+select * from tb_funcionaries where salario > 2000;
+
+select * from tb_funcionaries where salario < 2000;
+
+update tb_funcionaries set nome = "Marina" where id = 1;
+
+Select * from tb_funcionaries
+
+
+
+
